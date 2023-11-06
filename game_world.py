25e77850b4fe30aa45943afb2c81400f471a07a2
Jung Geuk Hun <111038432+rmrgns@users.noreply.github.com>
2023-11-06 14:55:44 +0900

    파일 추가

---
+++ game_world.py
@@ -0,0 +1,77 @@
+objects = [[] for _ in range(4)]
+# fill here
+# 충돌관점의 월드
+collision_pairs = {}    # {'boy:ball' : [[boy], [ball1, ball2, ball3, ...]
+
+def add_object(o, depth = 0):
+    objects[depth].append(o)
+
+def add_objects(ol, depth = 0):
+    objects[depth] += ol
+
+
+def update():
+    for layer in objects:
+        for o in layer:
+            o.update()
+
+
+def render():
+    for layer in objects:
+        for o in layer:
+            o.draw()
+
+# fill here
+def remove_collision_object(o):
+    for pairs in collision_pairs.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
+
+def remove_object(o):
+    for layer in objects:
+        if o in layer:
+            layer.remove(o)
+            remove_collision_object(o)
+            del o   # 객체를 메모리에서 제거
+            return
+    raise ValueError('Cannot delete non existing object')
+
+
+def clear():
+    for layer in objects:
+        layer.clear()
+
+
+
+# fill here
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
+def add_collision_pair(group, a, b):
+    if group not in collision_pairs:
+        print(f'Added new group {group}')
+        collision_pairs[group] = [ [], [] ]
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
+
+
+def handle_collisions():
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide(a, b):
+                    a.handle_collision(group, b)
+                    b.handle_collision(group, a)
+
